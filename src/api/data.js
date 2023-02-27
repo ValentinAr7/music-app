7d0fac3c75dc2e1cbbb40314b83752db6ec1f0a7
Valentin <valio990210@gmail.com>
2023-02-27 16:13:06 +0200

    save data for new albums

--- src/api/data.js
+++ src/api/data.js
@@ -1,23 +1,9 @@
 import { del, get, post, put} from "./api.js";
 
-export async function getAll(){         //read info from catalog.js
-    return get('/data/pets?sortBy=_createdOn%20desc&distinct=name')
+const endpoints = {
+    'albums' : '/data/albums'
 }
 
-
-export async function getById(id){
-    return get('/data/pets' + id)
-}
-
-
-export async function deleteById(id){
-    return del('/data/pets/' + id);
-}
-
-export async function createPet (petData){
-    return post('/data/pets/', petData)
-}
-
-export async function editPet(id, petData){
-    return put ('/data/pets' + id, petData)
+export async function createAlbum(data){
+        return post(endpoints.albums, data)
 }
