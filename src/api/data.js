c30d6240e2cdc0333a2b03e761cd43ac9030b8de
Valentin <valio990210@gmail.com>
2023-02-27 16:22:56 +0200

    data

--- src/api/data.js
+++ src/api/data.js
@@ -1,9 +1,14 @@
 import { del, get, post, put} from "./api.js";
 
 const endpoints = {
-    'albums' : '/data/albums'
+    'albums' : '/data/albums',
+    'getAllAlbums' : '/data/albums?sortBy=_createdOn%20desc&distinct=name'
 }
 
 export async function createAlbum(data){
         return post(endpoints.albums, data)
+}
+
+export async function getAllAlbums(){
+    return get(endpoints.getAllAlbums)
 }
