41b5df67dbbcea12e472d68a3e310f859c0d9283
Valentin <valio990210@gmail.com>
2023-02-28 12:18:59 +0200

    saerch albums data

--- src/views/search.js
+++ src/views/search.js
@@ -1,41 +1,49 @@
+import { searchAlbum } from "../api/data.js";
 import { html } from "../lib.js";
 
-const searchTem = () => html`
+const searchTem = (isClicked, handler) => html`
 <section id="searchPage">
 <h1>Search by Name</h1>
 
 <div class="search">
     <input id="search-input" type="text" name="search" placeholder="Enter desired albums's name">
-    <button class="button-list">Search</button>
+    <button @click=${handler} class="button-list">Search</button>
 </div>
 
 <h2>Results:</h2>
-
-<!--Show after click Search button-->
-<div class="search-result">
-    <!--If have matches-->
+${
+    isClicked ? 
+    html`<div class="search-result">
     <div class="card-box">
-        <img src="./images/BrandiCarlile.png">
-        <div>
-            <div class="text-center">
-                <p class="name">Name: In These Silent Days</p>
-                <p class="artist">Artist: Brandi Carlile</p>
-                <p class="genre">Genre: Low Country Sound Music</p>
-                <p class="price">Price: $12.80</p>
-                <p class="date">Release Date: October 1, 2021</p>
-            </div>
-            <div class="btn-group">
-                <a href="#" id="details">Details</a>
-            </div>
+    <img src="./images/BrandiCarlile.png">
+    <div>
+        <div class="text-center">
+            <p class="name">Name: In These Silent Days</p>
+            <p class="artist">Artist: Brandi Carlile</p>
+            <p class="genre">Genre: Low Country Sound Music</p>
+            <p class="price">Price: $12.80</p>
+            <p class="date">Release Date: October 1, 2021</p>
+        </div>
+        <div class="btn-group">
+            <a href="#" id="details">Details</a>
         </div>
     </div>
-
-    <!--If there are no matches-->
-    <p class="no-result">No result.</p>
+</div>` : 
+html` <p class="no-result">No result.</p>
+`
+}
 </div>
 </section>`
 
 
 export async function showSearch(ctx){
-    ctx.render(searchTem())
+    ctx.render(searchTem(false, onSearch))
+    async function onSearch(e){
+        const searchInput = document.getElementById('search-input');
+        const query = searchInput.value;
+        if(!query){
+            return alert('enter text')
+        }
+        const albums = await searchAlbum(query)
+    }
 }
